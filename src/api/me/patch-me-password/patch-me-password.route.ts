f08ceed5cde695849d15be7e7ef6dc188db60eb4
Ernesto Razo <razz.ernesto@gmail.com>
2026-04-27 12:27:26 +0800

    feat(me): implement user retrieval and password update routes with validation and error handling

--- src/api/me/patch-me-password/patch-me-password.route.ts
+++ src/api/me/patch-me-password/patch-me-password.route.ts
@@ -1,7 +1,6 @@
 import { createRoute, z } from "@hono/zod-openapi";
 import { PatchMePasswordSchema } from "../me.model.js";
 
-
 export const PatchMePasswordRoute = createRoute({
   path: '/password',
   method: 'patch',
