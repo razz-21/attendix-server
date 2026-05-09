03e2c7078a4d77450c5f0afcb4df6378138db04d
nel2131 <llvalmoria42157@liceo.edu.ph>
2026-05-09 13:45:59 +0800

    refactor(attendees): remove example values from attendee schemas

--- src/api/auth/auth.model.ts
+++ src/api/auth/auth.model.ts
@@ -5,11 +5,9 @@ export const TokenPayloadSchema = z.object({
   user: UserSchema.omit({ password: true }).openapi('User'),
   exp: z.number('Expires in is required').openapi({
     description: 'The expiration time of the token',
-    example: 15,
   }),
   refresh_exp: z.number('Refresh expires in is required').openapi({
     description: 'The expiration time of the refresh token',
-    example: 10080,
   }),
 }).openapi('TokenPayload');
 
@@ -18,29 +16,24 @@ export const EmailLoginSchema = z.object({
   .optional()
   .openapi({
       description: 'The email of the user',
-      example: 'john.doe@example.com',
   }),
   username: z.string()
   .trim()
   .optional()
   .openapi({
       description: 'The username of the user',
-      example: 'john.doe',
   }),
   password: z.string().openapi({
       description: 'The password of the user',
-      example: 'password',
   }),
 }).openapi('EmailLogin');
 
 export const EmailLoginResponseSchema = z.object({
   access_token: z.string('Token is required').openapi({
     description: 'The access token of the user',
-    example: 'token',
   }),
   refresh_token: z.string('Refresh token is required').openapi({
     description: 'The refresh token of the user',
-    example: 'refresh_token',
   }),
   payload: TokenPayloadSchema.openapi('Payload'),
 }).openapi('EmailLoginResponse');
--- src/api/workspace/workspace.model.ts
+++ src/api/workspace/workspace.model.ts
@@ -3,14 +3,12 @@ import { z } from "@hono/zod-openapi";
 export const WorkspaceSchema = z.object({
   id: z.uuidv4().default(crypto.randomUUID()).openapi({
     description: 'The unique identifier for the workspace',
-    example: '123e4567-e89b-12d3-a456-426614174000',
   }),
   name: z.string('Name is required')
     .trim()
     .min(1, 'Name is required')
     .openapi({
       description: 'The name of the workspace',
-      example: 'Workspace 1',
     }
   ),
   description: z.string()
@@ -18,26 +16,22 @@ export const WorkspaceSchema = z.object({
     .optional()
     .openapi({
       description: 'The description of the workspace',
-      example: 'This is a workspace description',
     }
   ),
   avatar: z.string()
     .trim()
     .openapi({
       description: 'The avatar of the workspace',
-      example: 'https://example.com/avatar.png',
     }
   ),
   created_at: z.iso.datetime().default(new Date().toISOString())
     .openapi({
       description: 'The date and time the workspace was created',
-      example: '2021-01-01T00:00:00.000Z',
     }
   ),
   updated_at: z.iso.datetime().default(new Date().toISOString())
     .openapi({
       description: 'The date and time the workspace was last updated',
-      example: '2021-01-01T00:00:00.000Z',
     }
   ),
 }).openapi('Workspace');
